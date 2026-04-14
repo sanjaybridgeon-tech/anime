ce18689d67b0ebb2ea0ab8f41b93c18ca78e42a3
Sanjay Atholi <sanjaysr2902@gmail.com>
2026-04-15 04:26:36 +0530

    fixed docker image

--- src/services/api.js
+++ src/services/api.js
@@ -3,4 +3,5 @@ const API = import.meta.env.VITE_API_URL;
 export const getProducts = async () => {
   const res = await fetch(`${API}/products`);
   return res.json();
+  console.log("API:", API);
 };
