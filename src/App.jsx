8d73d4854e109444e1498e63ff8e84e6c8a38aa7
Sanjay Atholi <sanjaysr2902@gmail.com>
2026-04-20 05:42:15 +0530

    last

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -10,26 +10,44 @@ import ChatBot from "./components/ChatBot";
 import Payment from "./pages/Payment";
 import ProductDetails from "./pages/ProductDetails";
 
+// 🔥 NEW COMPONENT (controls chatbot visibility)
+function Layout() {
+  const location = useLocation();
+
+  // ❌ pages where bot should be hidden
+  const hideBotRoutes = ["/login", "/register"];
+
+  const shouldHideBot = hideBotRoutes.includes(location.pathname);
+
+  return (
+    <>
+      <Navbar />
+
+      {/* 🤖 Show bot only if NOT login/register */}
+      {!shouldHideBot && <ChatBot />}
+
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <BrowserRouter>
-        <Navbar />
-        <ChatBot/>
-
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/products" element={<ProductList />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/payment" element={<Payment/>}/>
-            <Route path="/product/:id" element={<ProductDetails />} />
-          </Routes>
-        </div>
-
-        <Footer />
+        <Layout />
       </BrowserRouter>
     </div>
   );
