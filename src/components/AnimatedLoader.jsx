840105e76aabc10e3c0f63a4b5d6ead98934a6f3
Sanjay Atholi <sanjaysr2902@gmail.com>
2026-04-22 13:27:07 +0530

    last

--- src/components/AnimatedLoader.jsx
+++ src/components/AnimatedLoader.jsx
@@ -1,21 +1,14 @@
-import loaderVideo from "../assets/loader.mp4";
-
 function AnimatedLoader() {
   return (
     <div className="fixed inset-0 w-full h-full bg-black z-50">
-
       <video
-        src={loaderVideo}
+        src="/images/Loader.mp4"
         autoPlay
         loop
         muted
         playsInline
         className="w-full h-full object-cover"
       />
-
-      {/* Optional overlay (text) */}
-     
-
     </div>
   );
 }
